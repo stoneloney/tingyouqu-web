aeaf36160bfda705e37b373798f5124314041fdb
zhuwei <1428635858@qq.com>
2020-02-28 12:38:04 +0800

    02.28扫码

---
+++ js/scan.js
@@ -0,0 +1,66 @@
+$(function () {
+    $.getUrlParam = function (name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return decodeURI(r[2]); return null;
+    }
+
+    var orderno = $.getUrlParam('orderno');
+
+    $.ajax({
+        type: "post",
+        url: "http://chinaboatfun.com/api/member/ticketDetail",
+        dataType: "json",
+        data: {
+            orderno: orderno
+        },
+        success: function (res) {
+            console.log(res);
+            create1(res)
+            create2(res)
+            create3(res)
+            if(res.data.ticket.status == 0){
+                var stt = "<text class='piao'>" + '有效票' + "</text>"
+                $(".zt").append(stt);
+            }else{
+                var stt = "<text class='piao'>" + '无效票' + "</text>"
+                $(".zt").append(stt);
+            }
+        }
+
+    })
+
+    function create1(res) {
+        console.log(res.data.activity)
+        var activity = res.data.activity
+        var str = "<div>";
+        str += "<div class='h1'>" + activity.name + "</div>";
+        str += "<div class='shijian top'><img src='images/shijian.png' alt=''><div>" + activity.starttime + '～' + activity.endtime + "</div></div>"
+
+        str += "<div class='shijian top2'><img src='images/dizhi.png' alt=''><div>" + activity.province + '' + activity.city + '' + activity.address + "</div></div>"
+
+        str += "</div>";
+        $(".hed_a").append(str);
+    }
+
+    function create2(res) {
+        console.log(res.data.ticket)
+        var ticket = res.data.ticket
+        var str = "<div>";
+        str += "<div>" + '票种' + ':' + "<text>" + ticket.name + "</text></div>"
+        str += "<div>" + '票价' + ':' + "<text>" + ticket.price + "</text></div>"
+        str += "<div>" + '实付' + ':' + "<text>" + ticket.pay + '.00' + "</text></div>"
+        str += "</div>";
+
+      
+        $(".trunk_b").append(str);
+    }
+    function create3(res) {
+        console.log(res.data.user)
+        var user = res.data.user
+        var stt = "<text>" + user.realname + "</text>"
+        var sdd = "<text>" + user.phone + "</text>"
+        $(".xm").append(stt);
+        $(".dh").append(sdd);
+    }
+})
